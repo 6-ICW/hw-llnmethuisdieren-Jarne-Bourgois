02a2059a61f946ab3c44cb4bcf8a2d3ca01c8b4b
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-09-22 13:04:06 +0200

    commit 2

--- script/script.js
+++ script/script.js
@@ -6,4 +6,27 @@ const users = [
   { id: 3, naam: "Kleintjes", voornaam: "Steven" },
 ];
 
+const Form = document.getElementById("myForm");
+const Choice = document.getElementById("mySelect");
+const vullerlijst = document.getElementById("vullerlijst");
+const checkbox = document.getElementById("checkbox");
 
+Choice.addEventListener("change", () => {
+  vullerlijst.innerText = "";
+  let ChoiceValue = Choice.value - 1;
+  let tekst = document.createElement("p");
+  tekst.innerText = ` welkom ${users[(id = ChoiceValue)].voornaam} ${
+    users[(id = ChoiceValue)].naam
+  } `;
+  vullerlijst.appendChild(tekst);
+  console.log(tekst);
+  check();
+});
+
+function check() {
+  if (Choice.value >= 0) {
+    checkbox.style.display = "block";
+  } else {
+    checkbox.style.display = "none";
+  }
+}
